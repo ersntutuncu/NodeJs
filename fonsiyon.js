adccdec9267e7ff2d6793691dd62dd4bd22572e8
Ersin Tutuncu <ersntutuncu@gmail.com>
2018-07-06 23:22:00 +0300

    fonsiyonlar

---
+++ fonsiyon.js
@@ -0,0 +1,25 @@
+function ekle ()
+{
+    console.log ("deneme")
+}
+ekle();
+// parametresiz return edilmeyen fonksıyon
+function ekleme ()
+{
+    return "deneme return"
+}
+console.log("-------------")
+console.log(ekleme())
+
+function toplama (sayi ,ad )
+{
+    return sayi+2+"/"+ad
+}
+console.log("-----------------")
+console.log(toplama(3,"ersin"))//parametreli fonksıyon yapısı
+var fonksıyon=function()
+{
+    return "değişken tiplii fonskiyon";
+}
+console.log("----------------")
+console.log(fonksıyon())
